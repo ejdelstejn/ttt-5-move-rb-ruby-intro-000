28c6771b1fcd0489c55b999e8f7fe35f76fc1f39
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-01-20 18:15:49 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,6 +15,6 @@ def input_to_index(input)
 end
 
 def move(board, input_to_index, value = 'X')
-  board[input_to_index] = character
+  board[input_to_index] = value
 end
 # code your input_to_index and move method here!
